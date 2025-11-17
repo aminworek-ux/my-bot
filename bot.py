5818f8612e47e71012d777dd055863af56d71bc7
aminworek-ux <aminworek@gmail.com>
2025-11-17 22:13:44 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from telegram.ext import Updater, CommandHandler
 
-TOKEN = os.environ.get("TOKEN")  # این خط توکن را از Environment Variable میخونه
+TOKEN = os.environ.get("8328643964:AAFQLAk4ADPWQZaZ0_w9B7R9WCYKKkAx_4Q")  # این خط توکن را از Environment Variable میخونه
 
 def start(update, context):
     update.message.reply_text("ربات صرافی روشن شد ✔️")
